e44fade0dbaddca68186efa9ff1763ae445dd640
Maxim Korolyov <korolyov.maxim@gmail.com>
2019-08-23 13:20:00 +0300

    migrate to core

--- cmd/posts/main.go
+++ cmd/posts/main.go
@@ -2,72 +2,14 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"github.com/mkorolyov/core/config"
+	"github.com/mkorolyov/core/server"
 	"github.com/mkorolyov/posts"
-	"github.com/soheilhy/cmux"
-	"google.golang.org/grpc"
-	"log"
-	"net"
-	"net/http"
 )
 
 func main() {
-	// Create the main listener.
-	l, err := net.Listen("tcp", "0.0.0.0:9091")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Create a cmux.
-	m := cmux.New(l)
-
-	grpcL := m.Match(cmux.HTTP2())
-	httpL := m.Match(cmux.HTTP1Fast())
-
-	// Create your protocol servers.
-	grpcS := grpc.NewServer()
-	posts.RegisterPostsServer(grpcS, posts.NewService())
-
-	opts := []grpc.DialOption{grpc.WithInsecure()}
-	mux := runtime.NewServeMux()
-	if err := posts.RegisterPostsHandlerFromEndpoint(context.Background(), mux, "0.0.0.0:9091",
-		opts); err != nil {
-		panic(fmt.Sprint("failed to register http handler for service :%v", err))
-	}
-	httpS := &http.Server{
-		Handler: mux,
-	}
-
-	go func() {
-		if err := grpcS.Serve(grpcL); err != nil {
-			log.Fatalf("grpc server error %v", err)
-		}
-	}()
-
-	go func() {
-		if err := httpS.Serve(httpL); err != nil {
-			log.Fatalf("https server error %v", err)
-		}
-	}()
-
-	// Start serving!
-	if err := m.Serve(); err != nil {
-		log.Fatalf("cmux server error: %v", err)
-	}
+	postsService := posts.NewService()
+	loader := config.Configure()
+	s := server.New(loader, postsService)
+	s.Serve(context.Background())
 }
-
-//func main() {
-//	// Create the main listener.
-//	l, err := net.Listen("tcp", "0.0.0.0:9091")
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//
-//	grpcS := grpc.NewServer()
-//	posts.RegisterPostsServer(grpcS, posts.NewService())
-//
-//	if err := grpcS.Serve(l); err != nil {
-//		log.Fatalf("grpc serve error %v", err)
-//	}
-//}
--- service.go
+++ service.go
@@ -2,6 +2,8 @@ package posts
 
 import (
 	"context"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"google.golang.org/grpc"
 	"math/rand"
 	"strconv"
 	"time"
@@ -14,6 +16,17 @@ type PostService struct {
 func NewService() *PostService {
 	rand.Seed(time.Now().UnixNano())
 	return &PostService{storage: map[string][]*Post{}}
+}
+
+func (ps PostService) GRPCRegisterer() func(s *grpc.Server) {
+	return func(s *grpc.Server) {
+		RegisterPostsServer(s, ps)
+	}
+}
+
+func (ps PostService) HTTPRegisterer() func(ctx context.Context, mux *runtime.ServeMux, endpoint string,
+	opts []grpc.DialOption) (err error) {
+	return RegisterPostsHandlerFromEndpoint
 }
 
 func (ps PostService) Get(ctx context.Context, r *GetRequest) (*GetResponse, error) {
